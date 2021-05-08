2226ffe6a28d5bb3ef95a37a4f15df8494e74c3c
John Freeman <prsjohnny@gmail.com>
2021-05-08 11:44:21 -0400

    can now use Turbo::stream() instead of turbo_stream() if you want

--- src/Turbo.php
+++ src/Turbo.php
@@ -4,4 +4,9 @@ namespace JohnnyFreeman\LaravelTurbo;
 class Turbo
 {
     public const CONTENT_TYPE = 'text/vnd.turbo-stream.html';
+
+    public static function stream()
+    {
+        return new TurboStream;
+    }
 }
